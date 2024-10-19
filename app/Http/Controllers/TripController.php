8c3c364b54a93a601578a922246ab06bc364844a
Julius <ndaujulius@gmail.com>
2024-10-19 10:43:26 -0700

    update

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -38,4 +38,53 @@ class TripController extends Controller
 
         return response()->json(['message' => 'cannot find this trip'], 404);
     }
+
+    public function accept(Request $request, Trip $trip)
+    {
+        // a driver accepts a trip
+        $request->validate([
+            'driver_location' => 'required'
+        ]);
+
+        $trip->update([
+            'driver_id' => $request->user()->id,
+            'driver_location' => $request->driver_location
+        ]);
+
+        $trip->load('driver.user');
+    }
+    public function  start(Request $request, Trip $trip)
+    {
+        $trip->update([
+            'is_started' => true
+        ]);
+
+        $trip->load('driver.user');
+
+        return $trip;
+    }
+    public function  end(Request $request, Trip $trip)
+    {
+        $trip->update([
+            'is_comlete' => true
+        ]);
+
+        $trip->load('driver.user');
+
+        return $trip;
+    }
+    public function  location(Request $request, Trip $trip)
+    {
+        $request->validate([
+            'driver_location' => 'required'
+        ]);
+
+        $trip->update([
+            'driver_location' => $request->driver_location
+        ]);
+
+        $trip->load('driver.user');
+
+        return $trip;
+    }
 }
